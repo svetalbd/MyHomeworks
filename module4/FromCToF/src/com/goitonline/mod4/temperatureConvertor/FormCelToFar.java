8c072a92e9ff471538b3fb2d490078d14120bf7e
mykhailenko.s@ukr.net <mykhailenko.s@ukr.net>
2016-03-15 08:27:09 +0200

    All in one class

--- module4/FromCToF/src/com/goitonline/mod4/temperatureConvertor/FormCelToFar.java
+++ module4/FromCToF/src/com/goitonline/mod4/temperatureConvertor/FormCelToFar.java
@@ -12,28 +12,47 @@ public class FormCelToFar {
 
     public static void main (String[] args){
         while (exitValue) {
-            System.out.println("Make your choice: 1 - convert from Celsius to  Farrengeyt; 2 - from Farrengeyt to Celcium; 3 - break.");
+            System.out.println("\nMake your choice: 1 - convert from Celsius to  Farrengeyt; 2 - from Farrengeyt to Celcium; 3 - break.");
             Scanner sc = new Scanner(System.in);
             int choice = sc.nextInt();
             switch (choice) {
                 case 1:
-                    ConvertFromCToF convertFromCToF = new ConvertFromCToF();
-                    double resultCF = convertFromCToF.getResultOfConvert();
+                    double resultCF = readValue()*9d/5d+32d;
                     System.out.printf("The temperature according to Farrengeyt is %.3f\n", resultCF);
                     break;
                 case 2:
-                    ConvertFromFToC convertFromFToC = new ConvertFromFToC();
-                    double resultFC = convertFromFToC.getResultOfConvert();
+                    double resultFC = (readValue()-32d)*(5d/9d);
                     System.out.printf("The temperature according to Celsius is %.3f\n", resultFC);
                     break;
                 case 3:
                     exitValue = false;
                     break;
                 default:
-                    System.out.println("Unknown number");
+                    System.out.println("Unknown number\n");
                     break;
             }
         }
     }
+
+    private static double readValue (){
+        String newString;
+        Double value;
+
+        System.out.println("Input a temperature you need to convert: \n");
+        Scanner sc = new Scanner(System.in);
+
+        String phrase = sc.next();
+        String delims = "[ ]+";
+        String[] tokens = phrase.split(delims);
+
+        if (tokens[0].contains(",")) {
+            newString = tokens[0].replace(',', '.');
+        }
+        else newString = tokens[0];
+
+        value =  Double.parseDouble(newString);
+        return value;
+    }
+
 
 }
